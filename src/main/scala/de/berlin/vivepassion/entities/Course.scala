9d90c9085c0a23432e735b1f7e63e3fc661cf972
KrokodileDandy <s.fuehr1@gmail.com>
2019-08-04 20:17:48 +0200

    Added entities for courses and semesters.

---
+++ src/main/scala/de/berlin/vivepassion/entities/Course.scala
@@ -0,0 +1,5 @@
+package de.berlin.vivepassion.entities
+
+case class Course(id: Int, name: String) {
+  //TODO add ects points, description, professor
+}
---
+++ src/main/scala/de/berlin/vivepassion/entities/Semester.scala
@@ -0,0 +1,7 @@
+package de.berlin.vivepassion.entities
+
+import java.time.LocalDate
+
+case class Semester(id: Int, name: String, start: LocalDate, end: LocalDate) {
+
+}
